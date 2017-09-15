7ad9b12993a0e0fc3e2f74fa2301a51cbde0cf65
Souma <nkc.souma.kakami@gmail.com>
2017-09-15 11:15:20 +0900

    NotesとLaneを一つにまとめました、

---
+++ Assets/test/TouchManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour
+{
+    bool touched;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //押している最中は削除
+        if (touched)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// コライダーがクリック（タップ）されたときに呼び出される
+    /// </summary>
+    private void OnMouseDown()
+    {
+        Debug.Log("クリックされた" + gameObject.name);
+        touched = true;
+    }
+
+    /// <summary>
+    /// タップが終わったときに呼び出される
+    /// </summary>
+    private void OnMouseUp()
+    {
+        touched = false;
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
