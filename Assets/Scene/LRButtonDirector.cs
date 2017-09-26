347ce97d640779e37130cbaf11845023844f9fcf
Masato <nkc.Masato.Suzuki@gmail.com>
2017-09-26 11:03:31 +0900

    MusicSelectSceneいじった

---
+++ Assets/Scene/LRButtonDirector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LRButtonDirector: MonoBehaviour {
+
+    GameObject RButton;
+    GameObject LButton;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+    
+    }
+    public void RightButtonPush() {
+        RButton.transform.position = new Vector2(600, -60);
+    }
+    public void LeftButtonPush() {
+        LButton.transform.position = new Vector2(-600, -60);
+    }
+}
