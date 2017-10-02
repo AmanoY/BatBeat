34ed1ae2857469bb6672b3577ed114a1be6064b2
amanoY <nkc.yoshika.amano@gmail.com>
2017-10-02 12:42:23 +0900

    ｑｑ

---
+++ Assets/test/ResetVec.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetVec : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameData.Reset();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
